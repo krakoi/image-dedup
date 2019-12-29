068d5a38bf31de81c2ef096285cadb4cceafc4b6
Gyula Kisrákói <krakoi90@gmail.com>
2019-12-29 20:04:21 +0100

    refactor source files: create lib which is used by the main binary use clap for parsing commandline arguments, add different customization options

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
-use std::path::PathBuf;
+extern crate log;
+extern crate image;
+extern crate img_hash;
+extern crate globset;
+extern crate walkdir;
+extern crate rayon;
+
+use log::{debug,error};
+use std::path::{Path,PathBuf};
 use img_hash::{HasherConfig, Hasher, ImageHash};
 use walkdir::{WalkDir,DirEntry};
-use globset::GlobMatcher;
+use globset::{GlobMatcher, GlobBuilder};
 use rayon::prelude::*;
 
 type HashSize = [u8; 8];
+
+#[macro_export]
+macro_rules! DEFAULT_FILTER {
+  () => ( "*.{jpg,jpeg,png,gif,webp,bmp,tiff}" )
+}
 
 pub struct HashOf {
   hash: ImageHash<HashSize>,
@@ -21,7 +34,7 @@ fn filter_glob_errors<E: std::fmt::Debug>(file: Result<DirEntry,E>) -> Option<Pa
       }
     }
     Err(e) => {
-      eprintln!("Error during file matching: {:?}", e);
+      error!("Error during file matching: {:?}", e);
       None
     }
   }
@@ -31,7 +44,7 @@ fn hash_image(hasher: &mut Hasher<HashSize>, file: PathBuf) -> Option<HashOf> {
   match image::open(&file) {
     Ok(img) => {
       let hash = hasher.hash_image(&img);
-      println!("{:?} -> {}", file, hash.to_base64());
+      debug!("{:?} -> {}", file, hash.to_base64());
 
       Some(HashOf {
         hash,
@@ -39,13 +52,13 @@ fn hash_image(hasher: &mut Hasher<HashSize>, file: PathBuf) -> Option<HashOf> {
       })
     },
     Err(e) => {
-      eprintln!("Error decoding image {:?}: {:?}", file, e);
+      error!("Error decoding image {:?}: {:?}", file, e);
       None
     }
   }
 }
 
-pub fn calculate_hashes(tree: WalkDir, matcher: GlobMatcher) -> Vec<HashOf> {
+pub fn calculate_hashes_sequential(tree: WalkDir, matcher: GlobMatcher) -> Vec<HashOf> {
   let mut hasher = HasherConfig::with_bytes_type::<HashSize>().to_hasher();
 
   return tree.into_iter()
@@ -65,7 +78,22 @@ pub fn calculate_hashes_paralell(tree: WalkDir, matcher: GlobMatcher) -> Vec<Has
   .collect();
 }
 
-pub fn same_pairs(vec: &Vec<HashOf>, threshold: u32) -> Vec<(&String,&String)> {
+pub fn calculate_hashes(root: &Path, paralell: bool, custom_filter: Option<&str>) -> Vec<HashOf> {
+  let images = WalkDir::new(root);
+
+  let glob = GlobBuilder::new(custom_filter.unwrap_or(DEFAULT_FILTER!()))
+      .case_insensitive(true)
+      .build().expect("Failed to parse glob pattern")
+  .compile_matcher();
+
+  if paralell {
+    return calculate_hashes_paralell(images, glob)
+  } else {
+    return calculate_hashes_sequential(images, glob);
+  }
+}
+
+pub fn same_pairs(vec: &Vec<HashOf>, threshold: u32) -> Vec<(&str,&str)> {
   let mut pairs = Vec::new();
 
   for (i, a) in vec.iter().enumerate() {
@@ -73,7 +101,7 @@ pub fn same_pairs(vec: &Vec<HashOf>, threshold: u32) -> Vec<(&String,&String)> {
       let dist = a.hash.dist(&b.hash);
 
       if dist <= threshold {
-        pairs.push((&a.file, &b.file));
+        pairs.push((a.file.as_str(), b.file.as_str()));
       }
     }
   }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,44 @@
-extern crate image;
-extern crate img_hash;
+extern crate log;
+extern crate stderrlog;
+extern crate clap;
 extern crate globset;
 extern crate walkdir;
-extern crate rayon;
+extern crate image_dedup;
 
-mod hash;
-
-use std::env::{args, current_dir};
-use std::io::{Error, ErrorKind};
+use stderrlog::{Timestamp};
 use std::path::Path;
-use walkdir::WalkDir;
-use globset::GlobBuilder;
-use self::hash::{calculate_hashes, calculate_hashes_paralell, same_pairs};
+use image_dedup::{calculate_hashes, same_pairs, DEFAULT_FILTER};
+use clap::{clap_app, crate_version, crate_authors, crate_name, crate_description};
+
+static FILTER_HELP: &str = concat!("Filename pattern filter. Default: ", DEFAULT_FILTER!());
 
 fn main() -> Result<(), std::io::Error> {
-    let args: Vec<String> = args().collect();
-    let dir = Path::new(&args[1]);
+    let matches = clap_app!(myapp =>
+        (name: crate_name!())
+        (version: crate_version!())
+        (author: crate_authors!())
+        (about: crate_description!())
+        (@arg filter: -e --filter FILTER_HELP)
+        (@arg sequential: -s --seq "Turn off paralell execution - execution will be slower but somewhat less IO-intensive")
+        (@arg verbose: -v --verbose "Print file hashes")
+        (@arg DIR: +required "Sets the root directory, where images will be searched")
+    ).get_matches();
+
+    let dir = Path::new(matches.value_of("DIR").unwrap());
 
     if !dir.is_dir() {
         panic!("The given path is not a directory!");
     }
 
-    let images = WalkDir::new(dir);
+    stderrlog::new()
+        .module(module_path!())
+        .quiet(!matches.is_present("verbose"))
+        .verbosity(3)
+        .timestamp(Timestamp::Off)
+        .init()
+        .unwrap();
 
-    let glob = GlobBuilder::new("*.{jpg,jpeg,png,gif,webp,bmp,tiff}")
-        .case_insensitive(true)
-        .build().expect("Failed to parse glob pattern")
-    .compile_matcher();
-    
-    // let hashes = calculate_hashes(images);
-    let hashes = calculate_hashes_paralell(images, glob);
+    let hashes = calculate_hashes(dir, ! matches.is_present("sequential"), matches.value_of("filter"));
     let pairs = same_pairs(&hashes, 5);
 
     for (a, b) in pairs {
